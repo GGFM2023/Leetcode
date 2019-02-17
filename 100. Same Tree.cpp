8f4b92a7ac9051197b84226cda5309a31f2fced9
TianchanXU <43586362+TianchanXU@users.noreply.github.com>
2019-02-17 15:22:31 -0500

    Create 100. Same Tree.cpp

---
+++ 100. Same Tree.cpp
@@ -0,0 +1,26 @@
+/*
+Runtime: 4 ms, faster than 100.00% of C++ online submissions for Same Tree.
+Memory Usage: 10 MB, less than 100.00% of C++ online submissions for Same Tree.
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q) return true;
+        else if((!p && q) || (p && !q)) return false;
+        else if(p->val==q->val)
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        else return false;
+    }
+};
