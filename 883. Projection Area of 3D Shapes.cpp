de2c52e52a62a1fcf96fc180eba91ab71cfe9e47
TianchanXU <43586362+TianchanXU@users.noreply.github.com>
2019-03-01 18:45:05 -0500

    Create 883. Projection Area of 3D Shapes.cpp

---
+++ 883. Projection Area of 3D Shapes.cpp
@@ -0,0 +1,60 @@
+/*
+Runtime: 12 ms, faster than 100.00% of C++ online submissions for Projection Area of 3D Shapes.
+Memory Usage: 10.4 MB, less than 6.06% of C++ online submissions for Projection Area of 3D Shapes.
+
+N*N: x=y
+input array first layer = x
+            second layer = y
+
+exmample 2:
+four pillars
+
+xy projection = 4 // total no-zero entry
+xz projection = sum of the tallest pillar for every x
+yz projection = sum ot the tallest pillar for every y 
+*/
+
+class Solution {
+public:
+    int projectionArea(vector<vector<int>>& grid) {
+        int N=grid.size();
+        int xy=0;
+        int xz=0;
+        int yz=0;
+
+        for(auto i : grid){
+            for(int j : i){
+                if(j!=0) xy+=1;
+            }
+        }
+
+        for(auto i : grid){
+            int max = *max_element(i.begin(), i.end());
+            xz+=max;
+        }
+
+        int max=0;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                max=std::max(grid[j][i],max);
+            }
+            yz+=max;
+            max=0;
+        }
+
+        return xy+yz+xz;
+    }
+};
+
+/*
+N*N: x=y
+input array first layer = x
+            second layer = y
+
+exmample 2:
+four pillars
+
+xy projection = 4 // total no-zero entry
+xz projection = sum of the tallest pillar for every x
+yz projection = sum ot the tallest pillar for every y 
+*/
